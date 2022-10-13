351cdfd917c662060a5a78e929e81b3de3898594
M. Nurul Abser <absern46@gmail.com>
2022-10-13 14:29:00 +0600

    7th

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-
+// nav menu
 const hamburger = document.querySelector(".hamburger");
 let menuOpen = false;
 const navBar = document.querySelector(".nav-bar");
@@ -13,3 +13,18 @@ hamburger.addEventListener("click", () => {
     }
     navBar.classList.toggle("active");
 });
+
+// dropdown
+const dropdown = document.querySelector(".dropbtn");
+const dropdownContent = document.querySelector(".dropdown-content");
+dropdown.addEventListener("mouseover", function(){
+    dropdownContent.classList.add("expand");
+});
+
+document.addEventListener('click', function(event){
+    const dropdownHiding = document.querySelector('.dropdown-content');
+    if(!dropdownHiding.contains(event.target)) {
+       dropdownContent.classList.remove("expand");
+    }
+ });
+

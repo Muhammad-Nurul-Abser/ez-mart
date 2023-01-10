797b788dced73f82a8830757749690de160ffd36
M. Nurul Abser <absern46@gmail.com>
2023-01-10 13:58:54 +0600

    21th

--- js/nav.js
+++ js/nav.js
@@ -5,6 +5,10 @@ const navBar = document.querySelector(".nav-bar");
 const carousel = document.querySelector(".carousel");
 carousel.style.display = 'block';
 
+
+var delayInMilliseconds = 300; //1 second
+
+
 hamburger.addEventListener("click", () => {
     if (!menuOpen) {
         hamburger.classList.add("open");
@@ -13,7 +17,11 @@ hamburger.addEventListener("click", () => {
     } else {
         hamburger.classList.remove("open");
         menuOpen = false;
-        carousel.style.display = 'block';
+        setTimeout(function() {
+            //your code to be executed after 1 second
+            carousel.style.display = 'block';
+          }, delayInMilliseconds);
+
     }
     navBar.classList.toggle("active");
 });
